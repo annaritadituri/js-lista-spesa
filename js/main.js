227718d087b4be585040ea346de91296208f7145
annaritadituri <annaritadituri@icloud.com>
2024-05-06 16:08:44 +0200

    Shopping list added to ul

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@
 
 //////////////////////////////////////////////////////////
 
+let listaSpesa = ["Pane", "Pasta", "Carne", "Farina", "Latte", "Uova", "Biscotti", "Caffè", "Zucchero", "Prosciutto"];
+
+let ul = document.querySelector('ul');
+
+let i = 0;
+while (i < listaSpesa.length) {
+
+    let li = document.createElement('li');
+
+    li.append(listaSpesa[i]);
+    ul.append(li);
+
+    i++;
+}
